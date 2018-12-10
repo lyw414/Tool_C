991f8bd7c38399f2ca6a206930001ab25877e588
lyw414 <347528962@qq.com>
2018-12-10 16:38:28 +0800

    add log not finished

--- src/Log/include/Log.h
+++ src/Log/include/Log.h
@@ -1,5 +1,75 @@
 #ifndef LYW_CODE_LOG
 #define LYW_CODE_LOG
 #include <stdio.h>
+#include <pthread.h>
+
+typedef enum _Log_Level
+{
+    ERROR_LEVEL,
+    WARNING_LEVEL,
+    DEBUG_LEVEL
+
+} TLog_Level;
+
+
+typedef enum _Log_Mode
+{
+    SCK_MODE,
+    PTHREAD_MODE
+} TLog_Mode;
+
+typedef struct _Log_Info
+{    
+    FILE * file_hanlde; 
+    pthread_mutex_t lock;
+    TLog_Level level;
+    TLog_Mode mode;
+    unsigned int size;
+    char szLogPath[64];
+    char szLogName[128];
+    int port;
+    char ip[64];
+
+} TLog_Info, * PLog_Info;
+
+TLog_Info _g_log_info;
+PLog_Info _g_p_log_info = &_g_log_info;
+
+/*
+ * @breif       生成固定格的时间串
+ *
+ * @param[out]  输出时间串 
+ * @param[in]   输出时间串的size
+ *
+ * return       true 成功  false  失败
+ */
+bool Get_Str_time_for_log ( char * szStrTime, unsigned int iSizeOfStrTime );
+
+
+/*
+ * @breif            创建目录
+ * @param[in]        目录 
+ *
+ * @return           true 成功 false 失败 
+ */
+static bool mkdir_for_log(const char * szFilePath);
+
+
+bool create_file_for_log(const char * szFilePath,const char * szFileName);
+
+bool Init_Log(const char *szFileName, const char * szFilePath,TLog_Level level,unsigned int size,TLog_Mode mode);
+
+bool Free_Log();
+
+
+#define Init_Log(FileName,FilePath,size,mode) \
+    Init_Log(const char *szFileName, const char * szFilePath,unsigned int size,TLog_Mode mode);
+        
+#define UserError()
+#define UserWarning()
+#define UserDebug()
+#define UserError_ASC()
+#define UserWarning_ASC()
+#define UserDebug_ASC()
 
 #endif
--- src/Log/src/Log.c
+++ src/Log/src/Log.c
@@ -1,1 +1,158 @@
 #include "Log.h"
+/*
+ * @breif       生成固定格的时间串
+ * 
+ * @param[out]  输出时间串 
+ * @param[in]   输出时间串的size
+ * 
+ * return       true 成功  false  失败
+ * 
+ */
+bool Get_Str_time_for_log ( char * szStrTime, unsigned int iSizeOfStrTime )
+{
+    bool res = false;
+    int index = 0;
+    time_t t;
+    struct tm * ptm = NULL;
+    char * tmpbuf [ 16 ] = { 0 };
+    
+    if ( iSizeOfStrTime <= strlen ( "yyyy_mm_dd_hh_MM_ss" ) )
+    {
+        return false;
+    }
+    else
+    {
+        memset ( szStrTime, 0x00, iSizeOfStrTime );
+    }
+
+    t = time ( NULL );
+
+    if ( ( ptm = gmtime ( & t ) ) != NULL )
+    {
+        vsnprintf ( szString,iSizeOfStrTime,"%04d_%02d_%02d_%02d_%02d_%02d",
+                    ptm->tm_year + 1990 ,ptm->tm_mon,ptm->tm_mday,ptm->tm_hour,ptm->tm_min,ptm->tm_sec);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+/*
+ * @breif            创建目录
+ * @param[in]        目录 
+ *
+ * @return           true 成功 false 失败 
+ *
+ */
+static bool mkdir_for_log(const char * szFilePath);
+{
+    char DirName[1024] = { 0 };
+    if ( szFilePath == NULL )
+    {
+        return false;
+    }
+    else
+    {
+        strcpy(DirName, szFilePath);      
+    }
+     
+    strcpy(_g_log_info.szLogPath,DirName);
+
+    len = strlen(DirName);             
+
+    if('/' != DirName[len-1]) 
+    {  
+        strcat(DirName, "/"); 
+        len++;
+    }           
+    for(i=1; i<len; i++)      
+    {      
+        if('/' == DirName[i])      
+        {      
+            DirName[i] = '\0';      
+            if(access(DirName, F_OK) != 0)      
+            {      
+                if(mkdir(DirName, 0777) == -1)      
+                {       
+                    return false;       
+                }      
+            }    
+            DirName[i] = '/';      
+        }      
+    }    
+    return true;      
+}
+
+bool Init_Log ( const char *szFileName, const char * szFilePath, TLog_Level level, unsigned int size, TLog_Mode mode )
+{
+    bool res = false;
+    char * szDefaultPath = "./";
+    char * szDefaultName = "Default";
+    char szFullFileName [ 1024 ] = { 0 };
+    char szStrTime [ 64 ] = { 0 };
+
+    //create log directory,if failed use default
+    if (  ( mkdir_for_log ( szFilePath ) ) )
+    {
+        strcpy ( _g_log_info.szLogPath, szFilePath );
+    }
+    else
+    {
+        mkdir_for_log ( szDefaultPath );
+        strcpy ( _g_log_info.szLogPath, szDefaultPath );
+    }
+
+    strcat ( szFulleFileName, _g_log_info.szLogPath);
+    strcat ( szFulleFileName, "/");
+
+    // open log file , if failed use default
+    if ( szFileName == NULL )
+    {
+        strcat ( szFulleFileName,szFileName);
+    }
+    else
+    {
+        strcat ( szFulleFileName,szDefaultName);
+    }
+    
+    if ( Get_Str_time_for_log ( szStrTime, sizeof ( szStrTime ) ) )
+    {
+        strcat ( szFulleFileName,"_" );
+        strcat ( szFulleFileName,szStrTime );
+    }
+
+    strcpy ( _g_log_info.szLogName,szFullFileName);
+
+    if ( ( _g_log_info.file_hanlde = fopen ( _g_log_info.szLogName,"ab" ) ) != NULL )
+    {
+        return false;
+    }
+
+    //set loglevel
+    if ( level != ERROR_LEVEL && level != WARNING_LEVEL && level != DEBUG_LEVEL ) 
+    {
+        _g_log_info.level = ERROR_LEVEL;
+    }
+    else
+    {
+        _g_log_info.level = level;
+    }
+
+    //set size
+    _g_log_info.size = size;
+    
+    //set mode
+    if ( _g_log_info.mode != SCK_MODE && _g_long_info.mode != PTHREAD_MODE )
+    {
+        _g_log_info.mode = PTHREAD_MODE;
+    }
+    else
+    {
+        _g_log_info.mode = mode;
+    }
+    
+    //
+} 
